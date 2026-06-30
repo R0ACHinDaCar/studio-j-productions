12597cbd6df06c86fdbe3a44c3982c2df6455448
R0ACHinDaCar <sjake7082@gmail.com>
2026-06-30 02:34:55 -0400

    1.034

---
+++ app/services/page.tsx
@@ -0,0 +1,9 @@
+import Services from "@/components/Services";
+
+export default function ServicesPage() {
+  return (
+    <main style={{ paddingTop: "120px" }}>
+      <Services />
+    </main>
+  );
+}
