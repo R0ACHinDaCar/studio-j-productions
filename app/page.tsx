87d07dd0f819665dbd1f7e07f499a04c3a9a32b6
R0ACHinDaCar <sjake7082@gmail.com>
2026-06-29 23:45:24 -0400

    1.028

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,24 @@ import Image from "next/image";
 
 import Hero from "@/components/Hero";
 import FeaturedProjects from "@/components/FeaturedProjects";
+import Services from "@/components/Services";
+
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <FeaturedProjects />
+    </main>
+  );
+}
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <FeaturedProjects />
+      <Services preview />
     </main>
   );
 }
