3661384ed1b43017c1a9a3b5217317bcd6f44c3e
R0ACHinDaCar <sjake7082@gmail.com>
2026-06-30 21:44:51 -0400

    1.073

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,10 @@
 import Services from "@/components/Services";
 
 
+
 export default function ServicesPage() {
   return (
-    <main data-nav-theme="dark" style={{ paddingTop: "120px" }}>
+    <main data-nav-theme="dark" style={{ paddingTop: "120px", backgroundColor: "#111111" }}>
       <Services />
     </main>
   );
